27de7aad542fd0b722228c22765e2d059d0eae87
Karn Wong <karn@karnwong.me>
2025-07-23 17:15:02 +0700

    go: add writing

---
+++ cmd/writing.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"time"
+
+	"github.com/kahnwong/media-discord-rpc/discord"
+	"github.com/kahnwong/rich-go/client"
+	"github.com/rs/zerolog/log"
+
+	"github.com/spf13/cobra"
+)
+
+var writingCmd = &cobra.Command{
+	Use:   "writing",
+	Short: "Display writing activity",
+	Run: func(cmd *cobra.Command, args []string) {
+		for {
+			discord.SetActivity(client.ActivityTypes.Playing, "Writing a blog post", "", "https://github.com/kahnwong/dashboard-icons/blob/master/rpc/tea.png?raw=true")
+
+			log.Info().Msg("Writing...")
+			time.Sleep(60 * time.Second)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(writingCmd)
+}
